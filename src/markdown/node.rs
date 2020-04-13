f29e267cb0c1492afe950c2e150a1c1e16354df0
Hiromu Sugiura <h.sugiura0705@outlook.com>
2020-04-14 02:36:53 +0900

    impl Display for Node

--- src/markdown/node.rs
+++ src/markdown/node.rs
@@ -1,10 +1,30 @@
 use std::fmt;
+use latex2mathml::{latex_to_mathml, DisplayStyle};
 
 #[derive(Debug, Clone)]
-pub enum Node {}
+pub enum Node {
+    Text(String),
+    Emphasis(String),
+    Strong(String),
+    LaTeX(DisplayStyle, String),
+    Paragraph(Vec<Node>),
+}
 
 impl fmt::Display for Node {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "", )
+        match self {
+            Node::Text(text) => write!(f, "{}", text),
+            Node::Emphasis(text) => write!(f, "<em>{}</em>", text),
+            Node::Strong(text) => write!(f, "<strong>{}</strong>", text),
+            Node::LaTeX(style, text) => match latex_to_mathml(text, style.clone()) {
+                Ok(mathml) => write!(f, "{}", mathml),
+                Err(_)     => write!(f, "${}$", text),
+            },
+            Node::Paragraph(vec) => {
+                write!(f, r#"<div class="paragraph">{}</div>"#,
+                    vec.iter().map(|node| format!("{}", node)).collect::<String>()
+                )
+            },
+        }
     }
 }
