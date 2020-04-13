059bfd3391f19991a45cc80f763e67c362f7b8bb
Hiromu Sugiura <h.sugiura0705@outlook.com>
2020-04-14 03:09:01 +0900

    add Hyperlink and tests to Node

--- src/markdown/node.rs
+++ src/markdown/node.rs
@@ -6,6 +6,7 @@ pub enum Node {
     Text(String),
     Emphasis(String),
     Strong(String),
+    Hyperlink(String, String),
     LaTeX(DisplayStyle, String),
     Paragraph(Vec<Node>),
 }
@@ -16,6 +17,7 @@ impl fmt::Display for Node {
             Node::Text(text) => write!(f, "{}", text),
             Node::Emphasis(text) => write!(f, "<em>{}</em>", text),
             Node::Strong(text) => write!(f, "<strong>{}</strong>", text),
+            Node::Hyperlink(text, url) => write!(f, r#"<a href="{}">{}</a>"#, url, text),
             Node::LaTeX(style, text) => match latex_to_mathml(text, style.clone()) {
                 Ok(mathml) => write!(f, "{}", mathml),
                 Err(_)     => write!(f, "${}$", text),
@@ -25,6 +27,22 @@ impl fmt::Display for Node {
                     vec.iter().map(|node| format!("{}", node)).collect::<String>()
                 )
             },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_work() {
+        let problems = vec![
+            (Node::Emphasis("italic".to_owned()), "<em>italic</em>"),
+            (Node::Strong("bold".to_owned()), "<strong>bold</strong>"),
+        ];
+        for (problem, answer) in problems.iter() {
+            assert_eq!(&format!("{}", problem), answer);
         }
     }
 }
