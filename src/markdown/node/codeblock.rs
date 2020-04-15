c45ec5f2b4163d2993f67f47c627747ebd3ba0bb
jb083 <sg.jb115@outlook.com>
2020-04-15 21:11:39 +0900

    use syntect::html::ClassedHTMLGenerator

--- src/markdown/node/codeblock.rs
+++ src/markdown/node/codeblock.rs
@@ -1,33 +1,34 @@
 use syntect::{
     parsing::SyntaxSet,
-    highlighting::{ThemeSet},
-    html::{
-        highlighted_html_for_string,
-    },
+    html::ClassedHTMLGenerator,
 };
-
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Language {
     C,
+    Cpp,
+    Cs,
     Css,
     Html,
     Javascript,
     Python,
     Rust,
+    ShellScript,
     None,
 }
 impl Language {
     fn extension(&self) -> &'static str {
         match self {
             Language::C => "c",
+            Language::Cpp => "cpp",
+            Language::Cs => "cs",
             Language::Css => "css",
             Language::Html => "html",
             Language::Javascript => "js",
             Language::Python => "py",
             Language::Rust => "rs",
+            Language::ShellScript => "sh",
             Language::None => "",
-            
         }
     }
 }
@@ -37,12 +38,19 @@ pub fn parse(text: String) -> (Language, Option<String>, String) {
 
     let mut line = lines.next().unwrap()[3..].split(':');
     let lang = match line.next() {
+        Some("bash") => Language::ShellScript,
         Some("c") => Language::C,
+        Some("cpp") => Language::Cpp,
+        Some("c++") => Language::Cpp,
+        Some("cs") => Language::Cs,
         Some("css") => Language::Css,
         Some("html") => Language::Html,
         Some("javascript") => Language::Javascript,
         Some("python") => Language::Python,
         Some("rust") => Language::Rust,
+        Some("shell") => Language::ShellScript,
+        Some("shell-session") => Language::ShellScript,
+        Some("shell_session") => Language::ShellScript,
         _ => Language::None,
     };
     let filename = line.next().map(|fp| fp.to_owned());
@@ -55,14 +63,16 @@ pub fn parse(text: String) -> (Language, Option<String>, String) {
     }
     let content = content.join("\n");
     
-    let ps = SyntaxSet::load_defaults_newlines();
-    let ts = ThemeSet::load_defaults();
-
     let content = match lang {
         Language::None => content,
         lang => {
-            let syntax = ps.find_syntax_by_extension(lang.extension()).unwrap();
-            highlighted_html_for_string(&content, &ps, syntax, &ts.themes["base16-mocha.dark"])
+            let syntax_set = SyntaxSet::load_defaults_newlines();
+            let syntax = syntax_set.find_syntax_by_extension(lang.extension()).unwrap();
+            let mut html_generator = ClassedHTMLGenerator::new(&syntax, &syntax_set);
+            for line in content.lines() {
+                html_generator.parse_html_for_line(&line);
+            }
+            html_generator.finalize()
         },
     };
 
@@ -91,5 +101,6 @@ fn main() {
         let (lang, filename, _content) = parse(text);
         assert_eq!(lang, Language::Rust);
         assert_eq!(filename, Some("src/main.rs".to_owned()));
+        // assert_eq!(&content, r#"console.log("Hello, world!");"#);
     }
 }
