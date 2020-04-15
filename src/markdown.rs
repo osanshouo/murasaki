a36fa66b6fbba9067fe2aa845a9f455453d7d6e1
jb083 <sg.jb115@outlook.com>
2020-04-15 23:47:43 +0900

    add template

--- src/markdown.rs
+++ src/markdown.rs
@@ -25,4 +25,68 @@ impl Markdown {
         
         Ok(Markdown { path, date, title, tags, content })
     }
+
+    fn tags_to_html(&self) -> String {
+        self.tags.iter()
+            .map(|tag| {
+                let mut slug = tag.clone();
+                slug.make_ascii_lowercase();
+                format!(r#"<div class="tag"><a href="{{ROOT}}/tags/{}">{}</a></div>"#, slug, tag)
+            })
+            .collect()
+    }
+
+    fn date_to_html(&self) -> String {
+        match self.date {
+            Some(date) => format!("<time datetime={0}>{0}</time>", date),
+            None => String::new(),
+        }
+    }
+
+    pub fn to_html(&self, config: &crate::config::Config) -> String {
+        CONTENT_TEMPLATE
+            .replace("{{SITE NAME}}", &config.site_name)
+            .replace("{{LANG}}", &config.language)
+            .replace("{{TITLE}", &self.title)
+            .replace("{{AUTHOR}", &config.author)
+            .replace("{{YEAR}", &config.year)
+            .replace("{{TAGS}}", &self.tags_to_html())
+            .replace("{{DATE}}", &self.date_to_html())
+            .replace("{{CONTENT}}", "")
+            .replace("{{ROOT}}", &config.root)
+    }
 }
+
+const CONTENT_TEMPLATE: &str = r#"<!DOCTYPE html>
+<html lang="{{LANG}}">
+
+<head>
+  <meta charset="utf-8">
+  <title>{{TITLE}} - {{SITE NAME}</title>
+  <link rel="stylesheet" href="/article-page.css">
+</head>
+
+<body>
+  <header>
+    <div class="header-container"><a href="{{ROOT}}">{{SITE NAME}}</a></div>
+  </header>
+
+  <main class="main">
+<section class="article">
+  <header>
+    <h1 class="title">{{TITLE}}</h1>
+    <div class="tags-container">
+        {{TAGS}}
+        <div class="date">{{DATE}}</div>
+    </div>
+  </header>
+
+  CONTENT
+</section>
+  </main>
+
+  <footer>
+    <div class="footer-container">&copy;{{YEAR}} {{AUTHOR}}</div>
+  </footer>
+</html>
+"#;
